e68912daea88cd83e7f238293e7b90e226468bf8
cagrisahinoglu <cagri.sahinogluu@gmail.com>
2022-05-13 20:43:16 +0300

    Add Lexend font family

--- app/src/main/java/com/cagrisahinoglu/rickandmortycompose/MainActivity.kt
+++ app/src/main/java/com/cagrisahinoglu/rickandmortycompose/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.cagrisahinoglu.rickandmortycompose.characterListing.CharacterListingPage
 import com.cagrisahinoglu.rickandmortycompose.ui.theme.RickAndMortyComposeTheme
@@ -24,10 +28,20 @@ class MainActivity : ComponentActivity() {
             RickAndMortyComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    CharacterListingPage()
+                    Column(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        TopAppBar {
+                            Text(text = "Characters",
+                                textAlign = TextAlign.Center,
+                                modifier = Modifier.fillMaxWidth(),
+                                style = MaterialTheme.typography.body1
+                            )
+                        }
+                        CharacterListingPage()
+                    }
                 }
             }
         }
--- app/src/main/java/com/cagrisahinoglu/rickandmortycompose/ui/theme/Type.kt
+++ app/src/main/java/com/cagrisahinoglu/rickandmortycompose/ui/theme/Type.kt
@@ -2,27 +2,41 @@ package com.cagrisahinoglu.rickandmortycompose.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.cagrisahinoglu.rickandmortycompose.R
+
+val fonts = FontFamily(
+    Font(R.font.lexend_thin, FontWeight.Thin),
+    Font(R.font.lexend_extralight, FontWeight.ExtraLight),
+    Font(R.font.lexend_light, FontWeight.Light),
+    Font(R.font.lexend_regular, FontWeight.Normal),
+    Font(R.font.lexend_medium, FontWeight.Medium),
+    Font(R.font.lexend_semibold, FontWeight.SemiBold),
+    Font(R.font.lexend_bold, FontWeight.Bold),
+    Font(R.font.lexend_extrabold, FontWeight.ExtraBold),
+    Font(R.font.lexend_black, FontWeight.Black),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
+        fontFamily = fonts,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp
+    ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
+        fontFamily = fonts,
+        fontWeight = FontWeight.Normal,
         fontSize = 14.sp
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
-    )
-    */
+    ),
+    defaultFontFamily = fonts
 )
